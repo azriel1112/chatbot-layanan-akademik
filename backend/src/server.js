947554e471db311190e4e98d4458cb2b10a157bb
azriel1112 <azriel1112@gmail.com>
2026-05-03 00:25:28 +0700

    persiapan deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,7 +8,17 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:5173' }));
+// app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:5173' }));
+
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://chatbot-layanan-akademik.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
